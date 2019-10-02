eef813196bd4e559a163ccaf0e5adb17a3aa33db
Jhon <jhon.inalien@gmail.com>
2019-10-02 08:28:01 -0400

    build
    Build

---
+++ build/precache-manifest.521736cf20076e289f70945872540615.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2226543249305ad03a95768cc7cc5191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a96dd97316b88070d89",
+    "url": "/static/css/main.1f77342a.chunk.css"
+  },
+  {
+    "revision": "021305d12dd02575a82c",
+    "url": "/static/js/2.76f3625f.chunk.js"
+  },
+  {
+    "revision": "8a96dd97316b88070d89",
+    "url": "/static/js/main.30821d9e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
